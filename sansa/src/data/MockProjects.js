d9bc618d4b8f21722eb4cc183b3c2a6b582371df
BenedictoGeraldo <geraldodawa@gmail.com>
2025-10-12 17:04:35 +0700

    add dummy data

---
+++ sansa/src/data/MockProjects.js
@@ -0,0 +1,39 @@
+// src/data/mockProjects.js
+export const DUMMY_PROJECTS = [
+  {
+    id: 1,
+    deadline: '8 Sep 2025',
+    image: '/illustrations/ui-design.svg',
+    category: 'Design',
+    title: 'UI/UX Design Mobile App',
+    description: 'Mendesain ulang aplikasi e-learning agar lebih user-friendly.',
+    lastUpdate: '2 hours',
+  },
+  {
+    id: 2,
+    deadline: '22 Sep 2025',
+    image: '/illustrations/web-dev.svg',
+    category: 'Development',
+    title: 'Web Development',
+    description: 'Membangun website keuangan sederhana dengan fitur login, dashboard, dan visualisasi data.',
+    lastUpdate: '2 hours',
+  },
+  {
+    id: 3,
+    deadline: '9 Sep 2025',
+    image: '/illustrations/data-analyst.svg',
+    category: 'Science',
+    title: 'Data Analyst',
+    description: 'Mengolah data penjualan online untuk menemukan pola dan membuat strategi bisnis.',
+    lastUpdate: '2 hours',
+  },
+  {
+    id: 4,
+    deadline: '22 Sep 2025',
+    image: '/illustrations/community-service.svg',
+    category: 'Riset dan Sosial',
+    title: 'Pengabdian Masyarakat',
+    description: 'Membuat program literasi digital sederhana untuk membantu Usaha Mikro di desa.',
+    lastUpdate: '2 hours',
+  },
+];
