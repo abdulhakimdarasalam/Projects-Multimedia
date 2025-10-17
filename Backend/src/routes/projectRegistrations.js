97a9b94415ed56b2d4de6d26440747e375e4a935
Abdul <abdulhakim54362@gmail.com>
2025-10-17 22:18:32 +0700

    Refactor: ProjectRegistrations

--- Backend/src/routes/projectRegistrations.js
+++ Backend/src/routes/projectRegistrations.js
@@ -9,40 +9,28 @@ const {
 } = require("../middlewares/authMiddleware");
 
 router.get(
-  "/data",
+  "/",
   verifyToken,
   autoRefreshToken,
   projectRegistrationsController.getAllProjectRegistrations
 );
-router.get(
-  "/acc/data",
-  verifyToken,
-  autoRefreshToken,
-  projectRegistrationsController.getAllAcceptedProjectRegistrations
-);
-router.get(
-  "/rejected/data",
-  verifyToken,
-  autoRefreshToken,
-  projectRegistrationsController.getAllRejectedProjectRegistrations
-);
 
 router.post(
-  "/create",
+  "/",
   verifyToken,
   autoRefreshToken,
   projectRegistrationsController.createProjectRegistration
 );
 
 router.put(
-  "/acc/:id",
+  "/:id/accept",
   verifyToken,
   isAdmin,
   autoRefreshToken,
   projectRegistrationsController.acceptRegistration
 );
 router.put(
-  "/reject/:id",
+  "/:id/reject",
   verifyToken,
   isAdmin,
   autoRefreshToken,
