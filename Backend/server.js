d83b8e4f1d584bcb445489768a4d2c9c59b98fd4
BenedictoGeraldo <geraldodawa@gmail.com>
2025-11-03 06:41:04 +0700

    uppdate port on localhost:4000

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const app = require("./src/app");
 const sequelize = require("./src/config/db");
 require("./src/models"); // import semua model dari index.js
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 sequelize.sync({}).then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
